054ebeb897e6fa6599ff8f4dcb6d2e3b449f5d47
elitehacker03 <106663801+elitehacker03@users.noreply.github.com>
2022-06-19 20:20:59 +0530

    Update elitephisher.sh

--- elitephisher.sh
+++ elitephisher.sh
@@ -19,7 +19,7 @@ G="\033[1;96m"
 W="\e[1;0m"
 
 ## ANSI colors (FG & BG)
-RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+RED="$(printf '\033[33m')"  GREEN="$(printf '\033[33')"  ORANGE="$(printf '\033[32m')"  BLUE="$(printf '\033[35m')"
 MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
 REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
 MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
